63142729e95bcffd156fafcdbe3b4668193157cb
Andrea Phillips <andrea@agilityfeat.com>
2016-10-04 15:26:37 -0600

    Moved Setup UI to createToolbar method

--- js/opentok.js-annotations/src/acc-pack-annotation.js
+++ js/opentok.js-annotations/src/acc-pack-annotation.js
@@ -181,6 +181,8 @@
   };
 
   var _createToolbar = function (session, options, externalWindow) {
+    _setupUI();
+
     var toolbarId = _.property('toolbarId')(options) || 'toolbar';
     var items = _.property('toolbarItems')(options);
     var colors = _.property('colors')(options) || _palette;
@@ -421,7 +423,6 @@
     // init analytics logs
     _logAnalytics();
     _log(_logEventData.actionInitialize, _logEventData.variationSuccess);
-    _setupUI();
   };
 
   AnnotationAccPack.prototype = {
